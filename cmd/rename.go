05386cfbb4746bc69f993ba71c16509856be496d
Sabarna <sabarnabasak@gmail.com>
2026-04-19 15:50:23 +0530

    add switch command to switch to existing branch

---
+++ cmd/rename.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"gtool/internal/git"
+
+	"github.com/spf13/cobra"
+)
+
+// renameCmd represents the rename command
+var renameCmd = &cobra.Command{
+	Use:   "rename [old-name] [new-name]",
+	Short: "Rename a branch",
+	Args:  cobra.ExactArgs(2),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		oldName := args[0]
+		newName := args[1]
+		err := git.RunGitCommand("branch", "-m", oldName, newName)
+		if err != nil {
+			cmd.Println("❌ Failed to rename branch:", err)
+			return
+		}
+		cmd.Println("✅ Branch renamed:", oldName, "→", newName)
+	},
+}
+
+func init() {
+	branchCmd.AddCommand(renameCmd)
+}
